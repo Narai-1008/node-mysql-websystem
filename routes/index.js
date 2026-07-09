4baf170e5bbfc8795fe7054141a591b6921e07fd
Narai-1008 <j2328@matsue-ct.ac.jp>
2026-07-09 06:44:43 +0000

    適切なユーザIDに基づいてToDoタスクを追加、表示しよう

--- routes/index.js
+++ routes/index.js
@@ -2,40 +2,87 @@ const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
 
-router.get('/', function (req, res, next) {
+function ensureAuthenticated(req, res, next) {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/signin');
+  }
+  next();
+}
+
+/*router.get('/', function (req, res, next) {
   const isAuth = req.isAuthenticated();
-  knex("tasks")
-    .select("*")
+  if (!isAuth) {
+    return res.render('index', {
+      title: 'ToDo App',
+      todos: [],
+      isAuth: false,
+    });
+  }
+
+  knex('tasks')
+    .select('*')
+    .where({ user_id: req.user.id })
     .then(function (results) {
       res.render('index', {
         title: 'ToDo App',
         todos: results,
-        isAuth: isAuth,
+        isAuth: true,
       });
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
-        isAuth: isAuth,
+        isAuth: true,
         errorMessage: [err.sqlMessage],
       });
     });
 });
+*/
 
-router.post('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
   const isAuth = req.isAuthenticated();
+  if (isAuth) {
+    const userId = req.user.id;
+    knex("tasks")
+      .select("*")
+      .where({user_id: userId})
+      .then(function (results) {
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+          isAuth: isAuth,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+          isAuth: isAuth,
+          errorMessage: [err.sqlMessage],
+        });
+      });
+  } else {
+    res.render('index', {
+      title: 'ToDo App',
+      isAuth: isAuth,
+    });
+  }
+});
+
+router.post('/', ensureAuthenticated, function (req, res, next) {
+  const userId = req.user.id;
   const todo = req.body.add;
-  knex("tasks")
-    .insert({user_id: 1, content: todo})
+  knex('tasks')
+    .insert({ user_id: userId, content: todo })
     .then(function () {
-      res.redirect('/')
+      res.redirect('/');
     })
     .catch(function (err) {
       console.error(err);
       res.render('index', {
         title: 'ToDo App',
-        isAuth: isAuth,
+        isAuth: true,
         errorMessage: [err.sqlMessage],
       });
     });
